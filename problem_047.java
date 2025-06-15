4796f4f24c44655fc1e893c9a16996487d356639
prabath17 <prabathsuseela@gmail.com>
2025-06-15 10:13:20 +0530

    1 2 3 4 5 2 3 4 5 3 4 5 4 5 5

---
+++ problem_047.java
@@ -0,0 +1,12 @@
+public class problem_047 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<n+1;i++){
+            for(int j=i;j<=n;j++){
+                System.err.print(j+" ");
+            }
+            System.err.println("");
+        }
+    }
+    
+}

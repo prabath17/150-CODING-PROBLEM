7b06ec9985d61f1de2e08476bed25b9be16a7b35
prabath17 <prabathsuseela@gmail.com>
2025-06-15 14:59:59 +0530

    pyramid pattern

---
+++ problem_054.java
@@ -0,0 +1,15 @@
+public class problem_054 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>i;j--){
+                System.err.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+        System.out.print("*");
+    }
+            System.err.println("");
+        }
+    }
+    
+}

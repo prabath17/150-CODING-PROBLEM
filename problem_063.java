8b3d7a48e5bc394aba417fb1db02758cdafdbf3c
prabath17 <prabathsuseela@gmail.com>
2025-06-15 17:47:49 +0530

    Program to find the frequency count of every number in an array in descending order

--- problem_063.java
+++ problem_063.java
@@ -1,5 +1,7 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class problem_063 {
     public static void main(String[] args) {
@@ -9,7 +11,7 @@ public class problem_063 {
             freq.put(arr[i],freq.getOrDefault(arr[i], 0)+1);
 
         }
-        int [] a=new int[freq.size()];
+        /*int [] a=new int[freq.size()];
         int n=0;
         for(int i : freq.keySet()){
             a[n]=i;
@@ -18,6 +20,14 @@ public class problem_063 {
         Arrays.sort(a);
         for (int key : a) {
             System.out.println("Key: " + key + " → Value: " + freq.get(key));
+        }*/
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(freq.entrySet());
+
+        list.sort((a, b) -> b.getValue().compareTo(a.getValue())); // descending
+
+        // Print
+        for (Map.Entry<Integer, Integer> entry : list) {
+            System.out.println("Key: " + entry.getKey() + " → Value: " + entry.getValue());
         }
 
     }

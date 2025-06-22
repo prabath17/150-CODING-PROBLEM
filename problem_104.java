982a7c7f4f6d4ac643f4a2887b529be19ce68157
prabath17 <prabathsuseela@gmail.com>
2025-06-22 10:41:25 +0530

    Program to count the characters which has lower followed by higher

--- problem_104.java
+++ problem_104.java
@@ -1,6 +1,6 @@
 public class problem_104 {
     public static void main(String[] args) {
-        String s="aBcD";
+        String s="xYzAb";
         int count=0;
         for(int i=0;i<s.length()-1;i++){
             if (Character.isLowerCase(s.charAt(i)) && Character.isUpperCase(s.charAt(i + 1))){

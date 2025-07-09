f469b828950b2aa957336d319ea05c31d620c994
prabath17 <prabathsuseela@gmail.com>
2025-07-09 23:47:07 +0530

    Split a string on hyphens

---
+++ problem_114.java
@@ -0,0 +1,10 @@
+public class problem_114 {
+    public static void main(String[] args) {
+        String str="Emma-is-a-data-scientist";
+        String[] arr=str.split("-");
+        for(String i:arr){
+            System.out.println(i);
+        }
+
+    }
+}

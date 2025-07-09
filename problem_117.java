5bddc89f0039e30696a1325b1c5f6a06352e0503
prabath17 <prabathsuseela@gmail.com>
2025-07-09 23:49:09 +0530

    First Non-Repeated character in a String

---
+++ problem_117.java
@@ -0,0 +1,24 @@
+public class problem_117 {
+    public static void main(String[] args) {
+        String str="racecar";
+        for(int i=0;i<str.length()-1;i++){
+            int count =0;
+            char val_i=str.charAt(i);
+            
+            for(int j=0;j<str.length();j++){
+                
+                char val_j=str.charAt(j);
+                //System.out.println(val_i+"  "+val_j);
+                if (val_i==val_j){
+                    count++;
+                }
+                
+
+            }
+            if(count==1){
+                    System.out.println(val_i);
+                    break;
+                }
+        }
+    }
+}

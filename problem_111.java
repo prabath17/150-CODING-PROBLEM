e549e7ac93a59a316b76f7e2806324ce5a7e8302
prabath17 <prabathsuseela@gmail.com>
2025-07-09 23:45:15 +0530

    Number of words present in the given sentences

---
+++ problem_111.java
@@ -0,0 +1,7 @@
+public class problem_111{
+    public static void main(String[] args) {
+        String str="The quick brown fox jumps over the lazy dog";
+        String[] arr=str.split(" ");
+        System.out.println(arr.length);
+    }
+}
